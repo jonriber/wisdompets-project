a647ba187cb7f53f4323a2f7b41dd92913a3522b
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-19 16:44:23 -0300

    Init

---
+++ env_wisdompets/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\jonat\Documents\PROGRAMAÇÃO\django\wisdompets-project\env_wisdompets\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
