54e0fe57f4209bb6a31feb4396350aecbddc32c3
jonriber <49167364+jonriber@users.noreply.github.com>
2020-06-19 21:37:55 -0300

    templates updated

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from . models import Pet
+from . models import Pet,Vaccine
 
 @admin.register(Pet) #using decorator to register model on admin page
-class PedAdmin(admin.ModelAdmin):
+class PetAdmin(admin.ModelAdmin):
     list_display = ['name','species','breed','age','sex']
+
+@admin.register(Vaccine)
+class VacAdmin(admin.ModelAdmin):
+    list_display = ['name',]
